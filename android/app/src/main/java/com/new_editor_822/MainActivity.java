defea8ed8cc2b2cd5e0ea8164774a54950967e28
mulana <mulana82@gmail.com>
2020-09-04 22:01:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/new_editor_822/MainActivity.java
@@ -0,0 +1,15 @@
+package com.new_editor_822;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "new_editor_822";
+    }
+}
